40afd2107ef79db1777870a3b8592abb65b4da84
Mah <timohaarsentiev1@gmail.com>
2025-05-19 21:38:50 +0300

    Initialcommit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    int amount = 54029;
+    int bonus = amount / 20;
+    System.out.println("Количество полученных бонусов - " + bonus + "!");
+    }
+}
